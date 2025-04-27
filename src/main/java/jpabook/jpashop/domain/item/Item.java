82f9e6abc44b2756e1a2a94054f52b12000f23d6
Altair5869 <th0538@naver.com>
2025-04-27 16:02:44 +0900

    create business logic in Item entity & NotEnoughStockException

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,7 +13,6 @@ import java.util.List;
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "dtype")
 @Getter
-@Setter
 public abstract class Item {
 
     @Id
@@ -26,4 +26,28 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //== 비즈니스 로직 ==//
+    /*
+    * 엔티티에 비즈니스 로직을 추가하는 이유:
+    * 객체 지향적으로 생각하면 데이터를 가지고 있는 쪽에
+    * 비즈니스 메소드가 있는 것이 응집력이 있기 때문 + 관리 용이
+    * */
+
+    /*
+    * stock 증가
+    * */
+    public void addstock(int quantity){
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
+
+
 }
