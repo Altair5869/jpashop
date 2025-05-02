129a59f68033b7732bbc638327994ea694c8d142
Altair5869 <th0538@naver.com>
2025-05-02 17:10:30 +0900

    HomeController 생성 및 Home 화면 추가

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -5,6 +5,8 @@ import jpabook.jpashop.domain.Order;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 @RequiredArgsConstructor
 public class OrderRepository {
@@ -20,9 +22,16 @@ public class OrderRepository {
     }
 
 
-
-//    public List<Order> findAll(OrderSearch orderSearch) {
-//
-//    }
+//mybatis를 사용하는 이유 중 하나: 동적 쿼리 생성이 간편.
+    
+    public List<Order> findAll(OrderSearch orderSearch) {
+        return em.createQuery("select o from Order o join o.member m" +
+                    " where o.status = :status " +
+                    " and m.name like :name", Order.class)
+                .setParameter("status", orderSearch.getOrderStatus())
+                .setParameter("name", orderSearch.getMemberName())
+                .setMaxResults(1000) //최대 1000건
+                .getResultList();
+    }
 
 }
